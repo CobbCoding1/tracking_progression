ed6b01ceffdb0a725eb761045b907d6b8ebca79b
CobbCoding <rircobb@gmail.com>
2023-07-26 14:47:16 -0400

    Refactored Code, with a few comments. Converted to a Stateful Widget to allow for state updates on button press

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,50 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonText = "Click";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-              backgroundColor: Colors.blue,
-              title: const Text('Tracking Progression')),
-          body: Center(
-            child: Container(
-              child: const Text('Track Your Workouts!'),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
+        // AppBar
+        appBar: AppBar(
+          title: const Text("Hello"),
+        ),
+        // Body
+        body: Center(
+          child: ElevatedButton(
             onPressed: () {
-              print('Jeez!');
+              setState(() {
+                buttonText = "Clicked";
+              });
             },
-          )),
+            child: Text(buttonText),
+          ),
+        ),
+        // Bottom NavBar
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: "Settings",
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
